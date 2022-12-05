67194a0c750c91d29d2726c69a36e3cd132e6818
DiSSNiKO <nikokalanda@gmail.com>
2022-12-06 02:13:55 +0400

    pslwork

--- src/components/CartOverlay.js
+++ src/components/CartOverlay.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProductsInCart from './ProductsInCart';
-import ItemInCart from './ItemInCart';
 
 class CartOverlay extends React.Component {
     constructor(){
@@ -10,17 +9,17 @@ class CartOverlay extends React.Component {
             cartOverlayDisplayed: false
         }
     }
-    handleClick(){
-        if(this.state.cartOverlayDisplayed){
-            this.setState({
-                cartOverlayDisplayed: false
-            });
-        } else {
-            this.setState({
-                cartOverlayDisplayed: true
-            });
-            this.props.setPopUpWindowsClosed(false);
-        }
+    handleClick(e){
+            if(this.state.cartOverlayDisplayed){
+                this.setState({
+                    cartOverlayDisplayed: false
+                });
+            } else {
+                this.setState({
+                    cartOverlayDisplayed: true
+                });
+                this.props.setPopUpWindowsClosed(false);
+            }
     }
     componentDidUpdate(prevProps){
         if(this.props.popUpsClosed && this.props.popUpsClosed!=prevProps.popUpsClosed){
@@ -30,21 +29,21 @@ class CartOverlay extends React.Component {
         }
     }
     render(){
-        return <div className="cart-button-overlay-wrapper">
+        return <div className="cart-button-overlay-wrapper cart-overlay-part">
             <img src="/images/Empty Cart.svg" alt="" id="cart-button" onClick={this.handleClick.bind(this)}/>
-            <div className={`cart-overlay ${!this.state.cartOverlayDisplayed ? "no-display":""}`}>
-                <span className="my-bag"><strong>My Bag,</strong> 3 items{/*this.props.amount of items*/}</span>
-                <ProductsInCart />
-                <div className='cart-overlay-wrapper'>
-                    <span className='total-price-style'>Total</span>
-                    <span className="total-price total-price-style">69Shabangs</span>
+            <div className={`cart-overlay ${!this.state.cartOverlayDisplayed ? "no-display":""} cart-overlay-part`}>
+                <span className="my-bag cart-overlay-part"><strong className='cart-overlay-part'>My Bag,</strong> {this.props.cartItemObjects.length} items</span>
+                <ProductsInCart currentCurrency={this.props.currentCurrency} cartItemObjects={this.props.cartItemObjects}/>
+                <div className='cart-overlay-wrapper cart-overlay-part'>
+                    <span className='total-price-style cart-overlay-part'>Total</span>
+                    <span className="total-price total-price-style cart-overlay-part">69Shabangs</span>
                 </div>
-                <div className="cart-overlay-wrapper">
+                <div className="cart-overlay-wrapper cart-overlay-part">
                     <Link to={"/checkout"} className="to-checkout to-checkout-bag">VIEW BAG</Link>
                     <Link to={"/checkout"} className="to-checkout green-button-style">CHECK OUT</Link>
                 </div>
             </div>
-            <div className={`screen-dimmer ${!this.state.cartOverlayDisplayed ? "no-display":""}`}></div>
+            <div className={`screen-dimmer ${!this.state.cartOverlayDisplayed ? "no-display":""} cart-overlay-part`}></div>
         </div>
     }
 }
